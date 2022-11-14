3305b2e4d6f386f2fbd1a21f54fd9326bbf7e102
Martin J White <mwhite@berkeley.edu>
2022-11-14 15:37:29 -0800

    Use -O in wget.

--- maps/fetch_LRGxPlanck_v1_data.sh
+++ maps/fetch_LRGxPlanck_v1_data.sh
@@ -8,9 +8,7 @@ url=https://zenodo.org/record/5834378/files
 rm -f data.tgz?download=1 LRGxPlanck_v1_data.tgz
 rm -f maps.tgz?download=1 LRGxPlanck_v1_maps.tgz
 #
-wget ${url}/data.tgz?download=1
-mv data.tgz?download=1 LRGxPlanck_v1_data.tgz
+wget -O LRGxPlanck_v1_data.tgz ${url}/data.tgz?download=1
 #
-wget ${url}/maps.tgz?download=1
-mv maps.tgz?download=1 LRGxPlanck_v1_maps.tgz
+wget -O LRGxPlanck_v1_maps.tgz ${url}/maps.tgz?download=1
 #
