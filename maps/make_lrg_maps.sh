50abc3dd765a759df0a9956a436c56d94be75952
Martin White <martinjameswhite@users.noreply.github.com>
2022-11-14 19:37:48 -0800

    Example submit script.
    An example submit script using the DESI repo on NERSC's Perlmutter system.

---
+++ maps/make_lrg_maps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH -J LRG
+#SBATCH -t 0:45:00
+#SBATCH -N 4
+#SBATCH -o MakeLRGmaps.out
+#SBATCH -e MakeLRGmaps.err
+#SBATCH -q regular
+#SBATCH -C cpu
+#SBATCH -A desi
+#
+date
+source activate cmb
+#
+# Now run the code.
+export OMP_NUM_THREADS=32
+srun -n 16 -c 32 python make_lrg_maps.py 2
+#
+date
+#
