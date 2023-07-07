87f373944c7b23f62074f60d0de3fee6799f55d5
NoahSailer <nsailer9@gmail.com>
2023-07-07 16:18:25 -0700

    differentiable limber code

--- theory/background.py
+++ theory/background.py
@@ -9,12 +9,12 @@
 import numpy as np
 from classy import Class
 
-def classyBackground(self, thy_args, zs):
+def classyBackground(thy_args, zs):
    """
    Computes background quantities relevant for Limber integrals
    using CLASS. Returns OmM (~0.3), chistar (comoving dist [h/Mpc] 
-   to the surface of last scatter), Ez (H(z)/H0 evaluated on self.z), 
-   and chi (comoving distance [h/Mpc] evaluated on self.z).
+   to the surface of last scatter), Ez (H(z)/H0 evaluated on zs), 
+   and chi (comoving distance [h/Mpc] evaluated on zs).
    
    Parameters
    ----------
@@ -33,4 +33,27 @@ def classyBackground(self, thy_args, zs):
    Ez      = np.vectorize(cosmo.Hubble)(zs)/cosmo.Hubble(0.)
    chi     = np.vectorize(cosmo.comoving_distance)(zs)*cosmo.h()
    
+   return OmM,chistar,Ez,chi
+   
+import jax.numpy as jnp
+   
+def analyticBackground(thy_args, zs):
+   """
+   TO DO: MAKE THIS MORE ACCURATE, BUT FOR NOW 
+   THIS IS FINE FOR TESTING CODE
+   """
+   omb,omc,ns,As,H0 = thy_args
+   OmM = (omb+omc)/(H0/100.)**2.
+   def E_z(z): return jnp.sqrt(OmM*(1.+z)**3. + (1.-OmM))
+   Ez = E_z(zs)
+   # chistar obviously depends on cosmology, 
+   # but for now let's ignore that for the sake of testing
+   chistar = 9400. 
+   #def chi_z(z):
+   #   zint = jnp.linspace(0.,z,50)
+   #   return jnp.trapz(2997.92458/E_z(zint),zint)
+   #chi = jnp.array([chi_z(z) for z in zs])
+
+   # not even close, just want it to be fast for now
+   chi = 1000.*zs
    return OmM,chistar,Ez,chi
--- theory/limber.py
+++ theory/limber.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.integrate import simps
+from scipy.integrate   import simps
 from scipy.interpolate import interp1d
 from scipy.interpolate import InterpolatedUnivariateSpline as Spline
 
@@ -9,9 +9,9 @@ from scipy.interpolate import InterpolatedUnivariateSpline as Spline
 ##
 #################################
 
-def effectiveRedshift(chi, WA, WB, zOfchi):
+def effRedshift(chi, WA, WB, zOfchi):
    """
-   Computes the effective redshift.
+   Computes the effective redshift. Returns float.
    
    Parameters
    ----------
@@ -31,11 +31,9 @@ def effectiveRedshift(chi, WA, WB, zOfchi):
    numer      = np.trapz(integrand,x=chi)
    return numer/denom
    
-
-def limberWithoutEvolution(chi, WA, WB, pAB):
+def limberIntegral(chi, WA, WB, pAB):
    """
-   Computes the Limber integral assuming a redshift-
-   independent power spectrum.
+   Computes the Limber integral. Returns (L) ndarray.
    
    Parameters
    ----------
@@ -47,8 +45,8 @@ def limberWithoutEvolution(chi, WA, WB, pAB):
    WB: (N) ndarray
       projection function [h/Mpc] for tracer B
    pAB: (N,L) ndarray
-      pAB[:,i] corresponds to the power spectrum
-      evaluated at k = (l[i]+0.5)/chi
+      pAB[i,j] corresponds to the AB power spectrum
+      evaluated at k = (l[j]+0.5)/chi(z[i])
    """
    N,L = pAB.shape
    # create L copies of WA*WB/chi^2
@@ -57,47 +55,6 @@ def limberWithoutEvolution(chi, WA, WB, pAB):
    integrand *= pAB
    return np.trapz(integrand,x=chi,axis=0)
    
-   
-def limberWithEvolution(chi, WA, WB, pAB, chiEdges):
-   """
-   Computes the Limber integral, taking into account
-   the redshift-dependence of the power spectrum.
-   
-   Parameters
-   ----------
-   chi: (N) ndarray
-      comoving distance [Mpc/h]
-   WA: (N) ndarray
-      projection function [h/Mpc] for tracer A
-   WB: (N) ndarray
-      projection function [h/Mpc] for tracer B
-   pAB: (N,L,Z-1) ndarray
-      pAB[:,i,k] corresponds to the power spectrum
-      at redshift Z[k] evaluated at k = (l[i]+0.5)/chi
-   chiEdges: (Z) ndarray
-      edges of each 'redshift bin' in comoving
-      distance units
-      
-   Raises
-   ------
-   RuntimeError
-      If chiEdges extends beyond the range of chi
-   """
-   if (chiEdges[0]<chi[0]) or (chiEdges[-1]>chi[-1]):
-      s= 'chiEdges extends beyond the domain of chi'
-      raise RuntimeError(s)
-   
-   N,L,Z = pAB.shape
-   Z    += 1
-   res   = np.zeros(L)
-   for z in Z:
-      edgeLo = chiEdges[z]
-      edgeHi = chiEdges[z+1]
-      I = np.where((chi >= edgeLo) & (chi < edgeHi+1e-3))
-      res += limberWithoutEvolution(chi[I], WA[I], WB[I], pAB[I,:,z])
-   return res
-
-
 ####################################
 ##
 ## A class for computing Cgg and Ckg
@@ -160,33 +117,109 @@ class limb():
       self.Pgg         = Pgg
       self.Pmm         = Pmm
       self._background = background
-      # and fiducial cosmology
-      self.thy_fid = thy_fid
+      # store fiducial cosmology (and set "current cosmology" to fiducial)
+      self._thy_fid  = thy_fid
+      self._thy_args = thy_fid.copy()
+      # compute effective redshifts and set up fidicual kernels & power spectra
+      # if applicable (when the relevant theory codes are supplied)
+
+      # RIGHT NOW evaluate WILL JUST THROW AN ERROR IF PK CODES ARE NOT GIVEN, MAKE THAT ERROR HANDELING MORE CLEVER
+                          
       self.computeZeff()
-      
+      self.evaluate(thy_fid)
+
+                          
+   # Recompute effective redshifts whenever the 
+   # fiducial cosmology changes 
+   @property
+   def thy_fid(self): return self._thy_fid
+   @thy_fid.setter
+   def thy_fid(self, new_thy_fid):
+      self._thy_fid = new_thy_fid
+      self.computeZeff()
+   # or when the background theory code changes
    @property     
-   def background(self):
-      return self._background
-      
+   def background(self): return self._background
    @background.setter
    def background(self, newBackground):
       self._background = newBackground
       self.computeZeff()
-      
+
+   # When the cosmology changes, recompute
+   # projection kernels and power spectra
+   @property
+   def thy_args(self): return self._thy_args        
+   @thy_args.setter
+   def thy_args(self, new_thy_args):
+      if new_thy_args != self.thy_args:
+         self.evaluate(new_thy_args)
+         self._thy_args = new_thy_args
+
+    
    def computeZeff(self):
-      if self._background is None: self.zeff = None
-      # compute effective redshifts (for Pgg about the fiducial cosmology)  
-      OmM,chistar,Ez,chi = self._background(thy_fid,self.z)
-      Wk,Wg_clust,Wg_mag = self.projectionKernels(thy_fid)
-      zeff = lambda i: effectiveRedshift(chi, Wg_clust[:,i], Wg_clust[:,i], self.z)
+      """
+      Computes the effective redshift for each galaxy sample 
+      assuming the fiducial cosmology and saves them to 
+      self.zeff, which is a (Ng) ndarray. If no background 
+      theory has been supplied, sets self.zeff = None.
+      """
+      if self._background is None: 
+          print('No background code given, skipping zeff caclaultion.')
+          self.zeff = None ; return ''
+      OmM,chistar,Ez,chi = self.background(self.thy_fid,self.z)
+      _,Wg,_             = self.projectionKernels(self.thy_fid)
+      zeff = lambda i: effRedshift(chi, Wg[:,i], Wg[:,i], self.z)
       self.zeff = np.array([zeff(i) for i in range(self.Ng)])
 
+    
+   def evaluate(self, thy_args, verbose=True):
+      """
+      Computes background quantities, projection kernels,
+      and power spectra for a given cosmology. These
+      are stored as:
+      
+      self.chi          # comoving distance, (Nz) ndarray
+      self.Wk           # CMB lensing kernel, (Nz) ndarray
+      self.Wg_clust     # galaxy clustering kernels, (Nz,Ng) ndarray
+      self.Wg_mag       # galaxy magnification kernels, (Nz,Ng) ndarray
+      self.Pgm_eval     # Pgm tables at each effective z, (Ng,Nk,1+Nmono) ndarray
+      self.Pgg_eval     # Pgm tables at each effective z, (Ng,Nk,1+Nmono) ndarray
+      self.Pmm_eval     # Pmm evaluated at each z in self.z, (Nk,1+Nz) ndarray
 
+      Nmono is the number of monomials (e.g. 1, b1, b2, ...), which can in 
+      general be different for Pgm and Pgg. The "+1" is a column of ks.
+      
+      Parameters
+      ----------
+      thy_args: type can vary according to theory codes
+         cosmological inputs
+      verbose: bool, default=True
+         when True, prints message when theory code 
+         (Pgg, Pgm, Pmm, or background) is missing
+      """
+      def pr(s): 
+         if verbose: print(s)
+      # background
+      if self.background is None:
+          pr('No background code given, skipping evaluate'); return ''
+      _,_,_,self.chi                    = self.background(thy_args,self.z)
+      self.Wk,self.Wg_clust,self.Wg_mag = self.projectionKernels(thy_args)
+      # Pgm
+      if self.Pgm is None: pr('No Pgm provided, skipping Pgm calculation')
+      else: self.Pgm_eval = np.array([self.Pgm(thy_args,z) for z in self.zeff])
+      # Pgg
+      if self.Pgg is None: pr('No Pgg provided, skipping Pgg calculation')
+      else: self.Pgg_eval = np.array([self.Pgg(thy_args,z) for z in self.zeff])
+      # Pmm
+      if self.Pmm is None: pr('No Pmm provided, skipping Pmm calculation')
+      else: self.Pmm_eval = self.Pmm(thy_args,self.z)
+
+       
    def gridMe(self,x):
       """
       Places input on a (Nz,Ng) grid. If x is z-independent, 
       repeats Ng times. If x is galaxy-independent, repeats 
-      Nz times. If x is a float, repeats  Nz*Ng times. 
+      Nz times. If x is a float, repeats Nz*Ng times. 
       
       Parameters
       ----------
@@ -217,31 +250,6 @@ class limb():
          raise RuntimeError(s)
       
       
-   def background(self, thy_args):
-      """
-      Computes background quantities relevant for Limber integrals. 
-      Returns OmM (~0.3), chistar (comoving dist [h/Mpc] to the 
-      surface of last scatter), Ez (H(z)/H0 evaluated on self.z), 
-      and chi (comoving distance [h/Mpc] evaluated on self.z).
-      
-      Parameters
-      ----------
-      thy_args: dict
-         inputs to CLASS for computing background quantities
-      """
-      cosmo = Class()
-      cosmo.set(thy_args)
-      cosmo.compute()
-      
-      OmM     = cosmo.Omega0_m()
-      zstar   = cosmo.get_current_derived_parameters(['z_rec'])['z_rec']
-      chistar = cosmo.comoving_distance(zstar)*cosmo.h()
-      Ez      = np.vectorize(cosmo.Hubble)(self.z)/cosmo.Hubble(0.)
-      chi     = np.vectorize(cosmo.comoving_distance)(self.z)*cosmo.h()
-      
-      return OmM,chistar,Ez,chi
-      
-      
    def projectionKernels(self, thy_args):
       """
       Computes the projection kernels [h/Mpc] for CMB lensing 
@@ -253,10 +261,19 @@ class limb():
       
       Parameters
       ----------
-      thy_args: dict
-         inputs to CLASS for computing background quantities
+      thy_args: type can vary according to theory codes
+         cosmological inputs
+         
+      Raises
+      ------
+      RuntimeError
+         if self.background is None
       """
-      OmM,chistar,Ez,chi = self._background(thy_args,self.z)
+      if self.background is None:
+         s  = 'must provide a background code to compute projection kernels'
+         raise RuntimeError(s)
+         
+      OmM,chistar,Ez,chi = self.background(thy_args,self.z)
       H0                 = 100./299792.458 # [h/Mpc] units
       ## CMB lensing
       Wk  = 1.5*OmM*H0**2.*(1.+self.z)
@@ -278,10 +295,112 @@ class limb():
       
       return Wk,Wg_clust,Wg_mag
   
-   """
-   TODO: decide how I want to organize the Pk -> Cl calculation
-   
-   magnification bias should probably not be included as column in tables (it
-   multiplies b1,etc. and Cgg is quadratic in smag so no analytic marg). However
-   changing smag should always be fast, don't need to redo full Pk -> Cl calculation.
-   """
+  
+   def computeCkg(self, i, thy_args, lmax=500):
+      """
+      Computes the CMB lensing-galaxy cross-correlation
+      up to a specified lmax within the Limber approximation.
+      Returns a clustering contribution Ckg_clust in the form 
+      of a "monomial table", i.e. a (Nl,Nmono) ndarray, as well 
+      as the magnification contribution Ckg_mag, which is a (Nl) 
+      ndarray. Given a set of monomial coefficients "coeff", 
+      which is a (Nmono) ndarray, the full prediction is: 
+        Ckg = np.dot(Ckg_clust,coeff) + (5*s-2) * Ckg_mag
+      where s is the slop of the cumulative magnitude func.  
+
+      Parameters
+      ----------
+      i: int
+         index of galaxy sample, i.e. i = 0,1,...,Ng-1
+      thy_args: type can vary according to theory codes
+         cosmological inputs
+      lmax: int, default=500
+         maximum multipole (inclusive)
+      """
+      # Compute power spectra (if necessary) by 
+      # setting thy_args. The "kgrid" is defined
+      # such that kgrid[i,j] = (l[j]+0.5)/chi[i]
+      self.thy_args = thy_args
+      l     = np.arange(lmax+1) ; Nl = len(l)
+      kgrid = (np.tile(l+0.5,self.Nz)/np.repeat(self.chi,Nl)).reshape((self.Nz,Nl))
+       
+      # "clustering contribution"
+      k         = self.Pgm_eval[i][:,0]     # assume that fist col. = k
+      Pgm       = self.Pgm_eval[i][:,1:]    # (Nk,Nmono) ndarray
+      Nmono     = Pgm.shape[1]              # number of monomials
+      Ckg_clust = np.zeros((Nl,Nmono))
+      for j in range(Nmono):
+         pint = Spline(k,Pgm[:,j],ext=1)
+         Ckg_clust[:,j] = limberIntegral(self.chi, self.Wk, self.Wg_clust[:,i], pint(kgrid))
+      
+      # magnification contribution
+      k       = self.Pmm_eval[:,0]          # assume that fist col. = k
+      Pmm     = self.Pmm_eval[:,1:]         # (Nk,Nz) ndarray
+      Pgrid   = np.zeros((self.Nz,Nl))
+      for j in range(self.Nz):
+          Pgrid[j,:] = Spline(k,Pmm[:,j],ext=1)(kgrid[j,:])
+      Ckg_mag = limberIntegral(self.chi, self.Wk, self.Wg_mag[:,i], Pgrid)
+
+      return Ckg_clust, Ckg_mag
+
+    
+   def computeCgg(self, i, thy_args, lmax=500):
+      """
+      Computes the galaxy auto-correlation up to a specified 
+      lmax within the Limber approximation. Returns a clustering 
+      contribution Cgg_clust in the form of a "monomial table", i.e. 
+      a (Nl,Nmono_auto) ndarray, the linear magnification contribution 
+      Cgg_mag_lin, which is a (Nl,Nmono_cross) ndarray, and the 
+      quadratic magnification contribution Cgg_mag_quad, which is a
+      (Nl) ndarray. Given a set of monomial coefficients "coeff_a" and 
+      "coeff_x" for the auto and cross respectively, the full prediction is: 
+        Cgg = np.dot(Cgg_clust,coeff_a) + (5*s-2) * np.dot(Cgg_mag_lin,coeff_x) 
+              + (5*s-2)**2 * Cgg_mag_quad
+      where s is the slop of the cumulative magnitude func.  
+
+      Parameters
+      ----------
+      i: int
+         index of galaxy sample, i.e. i = 0,1,...,Ng-1
+      thy_args: type can vary according to theory codes
+         cosmological inputs
+      lmax: int, default=500
+         maximum multipole (inclusive)
+      """
+      # Compute power spectra (if necessary) by 
+      # setting thy_args. The "kgrid" is defined
+      # such that kgrid[i,j] = (l[j]+0.5)/chi[i]
+      self.thy_args = thy_args
+      l          = np.arange(lmax+1) ; Nl = len(l)
+      kgrid = (np.tile(l+0.5,self.Nz)/np.repeat(self.chi,Nl)).reshape((self.Nz,Nl))
+      
+      Wg_clust   = self.Wg_clust[:,i]           # (Nz) ndarray
+      Wg_mag     = self.Wg_mag[:,i]             # (Nz) ndarray
+      kgg        = self.Pgg_eval[i][:,0]        # assume that k = fist col
+      Pgg        = self.Pgg_eval[i][:,1:]       # monomials = (Nk,Nmono_auto) ndarray
+      kgm        = self.Pgm_eval[i][:,0]        # assume that k = fist col
+      Pgm        = self.Pgm_eval[i][:,1:]       # monomials = (Nk,Nmono_cros) ndarray
+      Nmono_auto = Pgg.shape[1]                 # number of monomials for auto
+      Nmono_cros = Pgm.shape[1]                 # number of monomials for cross
+      
+      # "clustering contribution"
+      Cgg_clust = np.zeros((Nl,Nmono_auto))
+      for j in range(Nmono_auto):
+         pint = Spline(kgg,Pgg[:,j],ext=1)
+         Cgg_clust[:,j] = limberIntegral(self.chi, Wg_clust, Wg_clust, pint(kgrid))
+         
+      # (linear) magnification contribution
+      Cgg_mag_lin = np.zeros((Nl,Nmono_cros))
+      for j in range(Nmono_cros):
+         pint = Spline(kgm,Pgm[:,j],ext=1)
+         Cgg_mag_lin[:,j] = 2.*limberIntegral(self.chi, Wg_clust, Wg_mag, pint(kgrid))
+       
+      # (quadratic) magnification contribution
+      k       = self.Pmm_eval[:,0]          # assume that fist col. = k
+      Pmm     = self.Pmm_eval[:,1:]         # (Nk,Nz) ndarray
+      Pgrid   = np.zeros((self.Nz,Nl))
+      for j in range(self.Nz):
+         Pgrid[j,:] = Spline(k,Pmm[:,j],ext=1)(kgrid[j,:])
+      Cgg_mag_quad = limberIntegral(self.chi, Wg_mag, Wg_mag, Pgrid)
+      
+      return Cgg_clust, Cgg_mag_lin, Cgg_mag_quad
