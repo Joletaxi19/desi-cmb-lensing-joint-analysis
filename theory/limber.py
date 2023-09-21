2747f8bb72924b20945d61f7752fa7a70e554cf8
NoahSailer <nsailer9@gmail.com>
2023-09-21 15:00:15 -0700

    fixed merge conflicts

--- theory/limber.py
+++ theory/limber.py
@@ -1,61 +1,8 @@
 import numpy as np
 from scipy.integrate   import simps
-<<<<<<< HEAD
 from scipy.interpolate import interp1d
 from scipy.interpolate import InterpolatedUnivariateSpline as Spline
-
-#################################
-##
-## Some general purpose functions
-##
-#################################
-
-def effRedshift(chi, WA, WB, zOfchi):
-   """
-   Computes the effective redshift. Returns float.
-   
-   Parameters
-   ----------
-   chi: (N) ndarray
-      comoving distance [Mpc/h]. chi[0] and chi[-1]
-      correspond to the integration limits. 
-   WA: (N) ndarray
-      projection function [h/Mpc] for tracer A
-   WB: (N) ndarray
-      projection function [h/Mpc] for tracer B
-   zOfchi: (N) ndarray
-      redshift as a function of comoving distance
-   """
-   integrand  = WA*WB/chi**2
-   denom      = np.trapz(integrand,x=chi)
-   integrand  = WA*WB*zOfchi/chi**2
-   numer      = np.trapz(integrand,x=chi)
-   return numer/denom
-   
-def limberIntegral(chi, WA, WB, pAB):
-   """
-   Computes the Limber integral. Returns (L) ndarray.
-   
-   Parameters
-   ----------
-   chi: (N) ndarray
-      comoving distance [Mpc/h]. chi[0] and chi[-1]
-      correspond to the integration limits. 
-   WA: (N) ndarray
-      projection function [h/Mpc] for tracer A
-   WB: (N) ndarray
-      projection function [h/Mpc] for tracer B
-   pAB: (N,L) ndarray
-      pAB[i,j] corresponds to the AB power spectrum
-      evaluated at k = (l[j]+0.5)/chi(z[i])
-   """
-   N,L = pAB.shape
-   # create L copies of WA*WB/chi^2
-   integrand  = WA*WB/chi**2.
-   integrand  = np.repeat(integrand,L).reshape((N,L))
-   integrand *= pAB
-   return np.trapz(integrand,x=chi,axis=0)
-   
+    
 ####################################
 ##
 ## A class for computing Cgg and Ckg
@@ -63,10 +10,6 @@ def limberIntegral(chi, WA, WB, pAB):
 ##
 ####################################
 
-=======
-from scipy.interpolate import InterpolatedUnivariateSpline as Spline
-
->>>>>>> 42ed741 (mask making scripts)
 class limb():
    """
    A class for computing Cgg and Ckg.
@@ -89,13 +32,9 @@ class limb():
    XXXXX
    """
    
-<<<<<<< HEAD
    def __init__(self, dNdz_fname, zmin=0.001, zmax=5., Nz=500,
                       Pgm=None, Pgg=None, Pmm=None, 
                       background=None, thy_fid={}):
-=======
-   def __init__(self, dNdz_fname, thy_fid, Pgm, Pgg, Pmm, background, lmax=1000, Nlval=64, zmin=0.001, zmax=2., Nz=50):
->>>>>>> 42ed741 (mask making scripts)
       """
       Parameters
       ----------
@@ -114,12 +53,12 @@ class limb():
       self.zmax  = zmax
       self.Nz    = Nz
       self.z     = np.linspace(zmin,zmax,Nz)
-<<<<<<< HEAD
+        
       # evaluate dNdz on regular grid and normalize it so 
       # that \int dN/dz dz = 1 for each galaxy sample
       self.dNdz  = interp1d(dNdz[:,0],dNdz[:,1:],axis=0,bounds_error=False,fill_value=0.)(self.z)
       self.dNdz  = self.dNdz.reshape((self.Nz,self.Ng))   # does nothing for Ng > 1
-=======
+
       self.l     = np.arange(lmax+1) 
       self.lval  = np.logspace(0,np.log10(lmax),Nlval)
       self.Nl    = len(self.l)
@@ -128,7 +67,6 @@ class limb():
       # that \int dN/dz dz = 1 for each galaxy sample
       self.dNdz  = np.zeros((self.Nz,self.Ng))
       for j in range(self.Ng): self.dNdz[:,j] = Spline(dNdz[:,0],dNdz[:,j+1],ext=1)(self.z)
->>>>>>> 42ed741 (mask making scripts)
       norm       = simps(self.dNdz, x=self.z, axis=0)     # (Ng) ndarray
       norm       = self.gridMe(norm)
       self.dNdz /= norm
@@ -136,16 +74,10 @@ class limb():
       self.Pgm         = Pgm
       self.Pgg         = Pgg
       self.Pmm         = Pmm
-<<<<<<< HEAD
-      self._background = background
+      self.background  = background
       # store fiducial cosmology (and set "current cosmology" to fiducial)
       self._thy_fid  = thy_fid
-      self._thy_args = thy_fid.copy()
-      # compute effective redshifts and set up fidicual kernels & power spectra
-      # if applicable (when the relevant theory codes are supplied)
-
-      # RIGHT NOW evaluate WILL JUST THROW AN ERROR IF PK CODES ARE NOT GIVEN, MAKE THAT ERROR HANDELING MORE CLEVER
-                          
+      # compute effective redshifts and set up fidicual kernels & power spectra        
       self.computeZeff()
       self.evaluate(thy_fid)
 
@@ -158,37 +90,11 @@ class limb():
    def thy_fid(self, new_thy_fid):
       self._thy_fid = new_thy_fid
       self.computeZeff()
-   # or when the background theory code changes
-   @property     
-   def background(self): return self._background
-   @background.setter
-   def background(self, newBackground):
-      self._background = newBackground
-      self.computeZeff()
 
-   # When the cosmology changes, recompute
-   # projection kernels and power spectra
-   @property
-   def thy_args(self): return self._thy_args        
-   @thy_args.setter
-   def thy_args(self, new_thy_args):
-      if new_thy_args != self.thy_args:
-         self.evaluate(new_thy_args)
-         self._thy_args = new_thy_args
-
-    
-=======
-      self.background = background
-      # store fiducial cosmology and compute effective redshifts
-      self.thy_fid  = thy_fid                   
-      self.computeZeff()
-                              
->>>>>>> 42ed741 (mask making scripts)
    def computeZeff(self):
       """
       Computes the effective redshift for each galaxy sample 
       assuming the fiducial cosmology and saves them to 
-<<<<<<< HEAD
       self.zeff, which is a (Ng) ndarray. If no background 
       theory has been supplied, sets self.zeff = None.
       """
@@ -197,31 +103,10 @@ class limb():
           self.zeff = None ; return ''
       OmM,chistar,Ez,chi = self.background(self.thy_fid,self.z)
       _,Wg,_             = self.projectionKernels(self.thy_fid)
-      zeff = lambda i: effRedshift(chi, Wg[:,i], Wg[:,i], self.z)
-      self.zeff = np.array([zeff(i) for i in range(self.Ng)])
-
-    
-   def evaluate(self, thy_args, verbose=True):
-      """
-      Computes background quantities, projection kernels,
-      and power spectra for a given cosmology. These
-      are stored as:
-      
-      self.chi          # comoving distance, (Nz) ndarray
-      self.Wk           # CMB lensing kernel, (Nz) ndarray
-      self.Wg_clust     # galaxy clustering kernels, (Nz,Ng) ndarray
-      self.Wg_mag       # galaxy magnification kernels, (Nz,Ng) ndarray
-      self.Pgm_eval     # Pgm tables at each effective z, (Ng,Nk,1+Nmono) ndarray
-      self.Pgg_eval     # Pgm tables at each effective z, (Ng,Nk,1+Nmono) ndarray
-      self.Pmm_eval     # Pmm evaluated at each z in self.z, (Nk,1+Nz) ndarray
-
-      Nmono is the number of monomials (e.g. 1, b1, b2, ...), which can in 
-=======
-      self.zeff, which is a (Ng) ndarray.
-      """
-      OmM,chistar,Ez,chi = self.background(self.thy_fid,self.z)
-      _,Wg,_             = self.projectionKernels(self.thy_fid)
-      zeff = lambda i: simps(Wg[:,i]**2*self.z/chi**2,x=chi)/simps(Wg[:,i]**2/chi**2,x=chi)
+      def zeff(i):
+         denom  = np.trapz(Wg[:,i]*Wg[:,i]/chi**2,x=chi)
+         numer  = np.trapz(Wg[:,i]*Wg[:,i]*self.z/chi**2,x=chi)
+         return numer/denom
       self.zeff = np.array([zeff(i) for i in range(self.Ng)])
 
     
@@ -239,35 +124,15 @@ class limb():
       Pmm_eval     # Pmm evaluated at each z in self.z, (Nk,1+Nz) ndarray
 
       Nmono is the number of monomials (e.g. 1, alpha0, ...), which can in 
->>>>>>> 42ed741 (mask making scripts)
       general be different for Pgm and Pgg. The "+1" is a column of ks.
       
       Parameters
       ----------
       thy_args: type can vary according to theory codes
          cosmological inputs
-<<<<<<< HEAD
       verbose: bool, default=True
          when True, prints message when theory code 
          (Pgg, Pgm, Pmm, or background) is missing
-      """
-      def pr(s): 
-         if verbose: print(s)
-      # background
-      if self.background is None:
-          pr('No background code given, skipping evaluate'); return ''
-      _,_,_,self.chi                    = self.background(thy_args,self.z)
-      self.Wk,self.Wg_clust,self.Wg_mag = self.projectionKernels(thy_args)
-      # Pgm
-      if self.Pgm is None: pr('No Pgm provided, skipping Pgm calculation')
-      else: self.Pgm_eval = np.array([self.Pgm(thy_args,z) for z in self.zeff])
-      # Pgg
-      if self.Pgg is None: pr('No Pgg provided, skipping Pgg calculation')
-      else: self.Pgg_eval = np.array([self.Pgg(thy_args,z) for z in self.zeff])
-      # Pmm
-      if self.Pmm is None: pr('No Pmm provided, skipping Pmm calculation')
-      else: self.Pmm_eval = self.Pmm(thy_args,self.z)
-=======
       """
       OmM,chistar,Ez,chi = self.background(thy_args,self.z)
       Wk,Wg_clust,Wg_mag = self.projectionKernels(thy_args,bkgrnd=[OmM,chistar,Ez,chi])
@@ -275,7 +140,6 @@ class limb():
       Pgg_eval = self.Pgg(thy_args,self.zeff[i])
       Pmm_eval = self.Pmm(thy_args,self.z)
       return chi,Wk,Wg_clust,Wg_mag,Pgm_eval,Pgg_eval,Pmm_eval
->>>>>>> 42ed741 (mask making scripts)
 
        
    def gridMe(self,x):
@@ -288,7 +152,6 @@ class limb():
       ----------
       x: float, (Nz) ndarray, OR (Ng) ndarray
          the input to be gridded
-<<<<<<< HEAD
          
       Raises
       ------
@@ -298,34 +161,17 @@ class limb():
       """
       if isinstance(x,float):
          return x*np.ones_like(self.dNdz)
-      if not isinstance(x,np.ndarray):
-         s = 'input must either be a float or 1D ndarray'
-         raise RuntimeError(s)
-      if len(list(x.shape))>1:
-         s = 'input should only have 1 dimension'
-         raise RuntimeError(s)
-=======
-      """
-      if isinstance(x,float):
-         return x*np.ones_like(self.dNdz)
->>>>>>> 42ed741 (mask making scripts)
       N = x.shape[0]
       if N == self.Ng:
          return np.tile(x,self.Nz).reshape((self.Nz,self.Ng))
       if N == self.Nz:
          return np.repeat(x,self.Ng).reshape((self.Nz,self.Ng))
-<<<<<<< HEAD
       else: 
          s = 'input must satisfy len = self.Ng or self.Nz'
          raise RuntimeError(s)
       
       
-   def projectionKernels(self, thy_args):
-=======
-      
-      
    def projectionKernels(self, thy_args, bkgrnd=None):
->>>>>>> 42ed741 (mask making scripts)
       """
       Computes the projection kernels [h/Mpc] for CMB lensing 
       and each galaxy sample. The CMB lensing kernel (Wk) is 
@@ -344,18 +190,15 @@ class limb():
       RuntimeError
          if self.background is None
       """
-<<<<<<< HEAD
       if self.background is None:
          s  = 'must provide a background code to compute projection kernels'
          raise RuntimeError(s)
          
       OmM,chistar,Ez,chi = self.background(thy_args,self.z)
       H0                 = 100./299792.458 # [h/Mpc] units
-=======
       if bkgrnd is None: OmM,chistar,Ez,chi = self.background(thy_args,self.z)
       else:              OmM,chistar,Ez,chi = bkgrnd
       H0 = 100./299792.458 # [h/Mpc] units
->>>>>>> 42ed741 (mask making scripts)
       ## CMB lensing
       Wk  = 1.5*OmM*H0**2.*(1.+self.z)
       Wk *= chi*(chistar-chi)/chistar
@@ -364,13 +207,10 @@ class limb():
       Wg_clust  = self.gridMe(H0*Ez) * self.dNdz  
       # magnification bias contribution
       def integrate_z_zstar(x):
-<<<<<<< HEAD
          # approximates the integral 
          # \int_z^{zstar} dz' x(z') 
          # with a Riemann sum
-=======
          # approximates \int_z^{zstar} dz' x(z') with a Riemann sum
->>>>>>> 42ed741 (mask making scripts)
          x = np.flip(x,axis=0)
          x = np.cumsum(x,axis=0) * (self.z[1]-self.z[0])
          return np.flip(x,axis=0)
@@ -379,118 +219,6 @@ class limb():
       Wg_mag *= self.gridMe(1.5*OmM*H0**2.*(1.+self.z))
       
       return Wk,Wg_clust,Wg_mag
-<<<<<<< HEAD
-  
-  
-   def computeCkg(self, i, thy_args, lmax=500):
-      """
-      Computes the CMB lensing-galaxy cross-correlation
-      up to a specified lmax within the Limber approximation.
-      Returns a clustering contribution Ckg_clust in the form 
-      of a "monomial table", i.e. a (Nl,Nmono) ndarray, as well 
-      as the magnification contribution Ckg_mag, which is a (Nl) 
-      ndarray. Given a set of monomial coefficients "coeff", 
-      which is a (Nmono) ndarray, the full prediction is: 
-        Ckg = np.dot(Ckg_clust,coeff) + (5*s-2) * Ckg_mag
-      where s is the slop of the cumulative magnitude func.  
-
-      Parameters
-      ----------
-      i: int
-         index of galaxy sample, i.e. i = 0,1,...,Ng-1
-      thy_args: type can vary according to theory codes
-         cosmological inputs
-      lmax: int, default=500
-         maximum multipole (inclusive)
-      """
-      # Compute power spectra (if necessary) by 
-      # setting thy_args. The "kgrid" is defined
-      # such that kgrid[i,j] = (l[j]+0.5)/chi[i]
-      self.thy_args = thy_args
-      l     = np.arange(lmax+1) ; Nl = len(l)
-      kgrid = (np.tile(l+0.5,self.Nz)/np.repeat(self.chi,Nl)).reshape((self.Nz,Nl))
-       
-      # "clustering contribution"
-      k         = self.Pgm_eval[i][:,0]     # assume that fist col. = k
-      Pgm       = self.Pgm_eval[i][:,1:]    # (Nk,Nmono) ndarray
-      Nmono     = Pgm.shape[1]              # number of monomials
-      Ckg_clust = np.zeros((Nl,Nmono))
-      for j in range(Nmono):
-         pint = Spline(k,Pgm[:,j],ext=1)
-         Ckg_clust[:,j] = limberIntegral(self.chi, self.Wk, self.Wg_clust[:,i], pint(kgrid))
-      
-      # magnification contribution
-      k       = self.Pmm_eval[:,0]          # assume that fist col. = k
-      Pmm     = self.Pmm_eval[:,1:]         # (Nk,Nz) ndarray
-      Pgrid   = np.zeros((self.Nz,Nl))
-      for j in range(self.Nz):
-          Pgrid[j,:] = Spline(k,Pmm[:,j],ext=1)(kgrid[j,:])
-      Ckg_mag = limberIntegral(self.chi, self.Wk, self.Wg_mag[:,i], Pgrid)
-
-      return Ckg_clust, Ckg_mag
-
-    
-   def computeCgg(self, i, thy_args, lmax=500):
-      """
-      Computes the galaxy auto-correlation up to a specified 
-      lmax within the Limber approximation. Returns a clustering 
-      contribution Cgg_clust in the form of a "monomial table", i.e. 
-      a (Nl,Nmono_auto) ndarray, the linear magnification contribution 
-      Cgg_mag_lin, which is a (Nl,Nmono_cross) ndarray, and the 
-      quadratic magnification contribution Cgg_mag_quad, which is a
-      (Nl) ndarray. Given a set of monomial coefficients "coeff_a" and 
-      "coeff_x" for the auto and cross respectively, the full prediction is: 
-        Cgg = np.dot(Cgg_clust,coeff_a) + (5*s-2) * np.dot(Cgg_mag_lin,coeff_x) 
-              + (5*s-2)**2 * Cgg_mag_quad
-      where s is the slop of the cumulative magnitude func.  
-
-      Parameters
-      ----------
-      i: int
-         index of galaxy sample, i.e. i = 0,1,...,Ng-1
-      thy_args: type can vary according to theory codes
-         cosmological inputs
-      lmax: int, default=500
-         maximum multipole (inclusive)
-      """
-      # Compute power spectra (if necessary) by 
-      # setting thy_args. The "kgrid" is defined
-      # such that kgrid[i,j] = (l[j]+0.5)/chi[i]
-      self.thy_args = thy_args
-      l          = np.arange(lmax+1) ; Nl = len(l)
-      kgrid = (np.tile(l+0.5,self.Nz)/np.repeat(self.chi,Nl)).reshape((self.Nz,Nl))
-      
-      Wg_clust   = self.Wg_clust[:,i]           # (Nz) ndarray
-      Wg_mag     = self.Wg_mag[:,i]             # (Nz) ndarray
-      kgg        = self.Pgg_eval[i][:,0]        # assume that k = fist col
-      Pgg        = self.Pgg_eval[i][:,1:]       # monomials = (Nk,Nmono_auto) ndarray
-      kgm        = self.Pgm_eval[i][:,0]        # assume that k = fist col
-      Pgm        = self.Pgm_eval[i][:,1:]       # monomials = (Nk,Nmono_cros) ndarray
-      Nmono_auto = Pgg.shape[1]                 # number of monomials for auto
-      Nmono_cros = Pgm.shape[1]                 # number of monomials for cross
-      
-      # "clustering contribution"
-      Cgg_clust = np.zeros((Nl,Nmono_auto))
-      for j in range(Nmono_auto):
-         pint = Spline(kgg,Pgg[:,j],ext=1)
-         Cgg_clust[:,j] = limberIntegral(self.chi, Wg_clust, Wg_clust, pint(kgrid))
-         
-      # (linear) magnification contribution
-      Cgg_mag_lin = np.zeros((Nl,Nmono_cros))
-      for j in range(Nmono_cros):
-         pint = Spline(kgm,Pgm[:,j],ext=1)
-         Cgg_mag_lin[:,j] = 2.*limberIntegral(self.chi, Wg_clust, Wg_mag, pint(kgrid))
-       
-      # (quadratic) magnification contribution
-      k       = self.Pmm_eval[:,0]          # assume that fist col. = k
-      Pmm     = self.Pmm_eval[:,1:]         # (Nk,Nz) ndarray
-      Pgrid   = np.zeros((self.Nz,Nl))
-      for j in range(self.Nz):
-         Pgrid[j,:] = Spline(k,Pmm[:,j],ext=1)(kgrid[j,:])
-      Cgg_mag_quad = limberIntegral(self.chi, Wg_mag, Wg_mag, Pgrid)
-      
-      return Cgg_clust, Cgg_mag_lin, Cgg_mag_quad
-=======
 
 
    def computeCggCkg(self, i, thy_args, smag, ext=3):
@@ -555,4 +283,3 @@ class limb():
          Ckg[:,j+1+Nmono_auto] = Spline(self.lval,integral)(self.l)
           
       return Cgg,Ckg
->>>>>>> 42ed741 (mask making scripts)
--- theory/pkCodes.py
+++ theory/pkCodes.py
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-# This is a wrapper around various cosmology/PT/emulator codes. This file contains 
-# several methods to compute real-space power spectra. Each method should have 
-# (thy_args, z) as its first two arguments, and can optionally have trailing arguments. 
-# These methods return power spectrum tables, which are then multiplied by biases, 
-# counterterms, etc. to return the full theory prediction.
-#
-#
-# Currently wrapped:
-# - Pgg with velocileptors
-# - Pgm with velocileptors
-# - Pmm with HaloFit
-# - Pmm with Aemulus emulator
-
-# ingredients
-=======
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# UPDATE THE DOCUMENTATION
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
 # This is a wrapper around various cosmology/PT/emulator codes. This file contains 
 # several methods to compute real-space power spectra. Each method should have 
 # (thy_args, z) as its arguments. These methods return power spectrum tables.
@@ -30,21 +9,17 @@
 
 # ingredients
 import sys
->>>>>>> 42ed741 (mask making scripts)
 import numpy as np
 from classy import Class
 from velocileptors.LPT.cleft_fftw import CLEFT
 
-<<<<<<< HEAD
 # fiducial k-grid [h/Mpc] on which we evaluate (Pgm,Pgg,Pmm) tables
-=======
 # sorry about the hard-coded paths for now...
 sys.path.append('/pscratch/sd/n/nsailer/aemulus_heft') 
 from aemulus_heft.heft_emu import NNHEFTEmulator
 nnemu = NNHEFTEmulator()
 
 # fiducial k-grid [h/Mpc] on which we evaluate (Pgm,Pgg) tables for velocileptors
->>>>>>> 42ed741 (mask making scripts)
 ks = np.concatenate( ([0.0005,],\
                         np.logspace(np.log10(0.0015),np.log10(0.029),60, endpoint=True),\
                         np.arange(0.03,0.51,0.01),\
@@ -59,7 +34,6 @@ def getCosmo(thy_args):
    thy_args: dict
       cosmological inputs to CLASS
    """
-<<<<<<< HEAD
    params = {'output': 'mPk','P_k_max_h/Mpc': 20.,'non linear':'halofit','z_pk': '0.0,20',
              'N_ur': 1.0196,'N_ncdm': 2,'m_ncdm': '0.01,0.05'}
    cosmo = Class()
@@ -69,22 +43,7 @@ def getCosmo(thy_args):
    return cosmo
 
 
-def pmmHalofit(thy_args,z,k=None):
-=======
-   omb,omc,ns,ln10As,H0,Mnu = thy_args[:6]
-             
-   params = {'output': 'mPk','P_k_max_h/Mpc': 20.,'non linear':'halofit', 
-             'z_pk': '0.0,20','A_s': 1e-10*np.exp(ln10As),'n_s': ns,'h': H0/100., 
-             'N_ur': 2.0328,'N_ncdm': 1,'m_ncdm': Mnu,'tau_reio': 0.0568,
-             'omega_b': omb,'omega_cdm': omc}
-             
-   cosmo = Class()
-   cosmo.set(params)
-   cosmo.compute()
-   return cosmo
-
 def pmmHalofit(thy_args,z):
->>>>>>> 42ed741 (mask making scripts)
    """
    Returns a table with shape (Nk,2). The 
    first column is k, while the second column
@@ -101,11 +60,8 @@ def pmmHalofit(thy_args,z):
       wavevectors [h/Mpc] on which to evaluate
       the power spectrum table
    """
-<<<<<<< HEAD
    if k is None: k = ks
-=======
    k = np.logspace(np.log10(0.005),np.log10(5.),200)
->>>>>>> 42ed741 (mask making scripts)
    cosmo = getCosmo(thy_args)
    h = cosmo.h()
    Pk = lambda zz: np.array([cosmo.pk(kk*h,zz)*h**3 for kk in k])
@@ -113,10 +69,7 @@ def pmmHalofit(thy_args,z):
    else: res = [Pk(zz) for zz in z]
    return np.array([k]+res).T
 
-<<<<<<< HEAD
-   
-=======
->>>>>>> 42ed741 (mask making scripts)
+
 def pggVelocileptors(thy_args,z,k=None):
    """
    Returns a Pgg table with shape (Nk,15).
@@ -138,10 +91,7 @@ def pggVelocileptors(thy_args,z,k=None):
       wavevectors [h/Mpc] on which to evaluate
       the power spectrum table
    """
-<<<<<<< HEAD
-=======
    omb,omc,ns,ln10As,H0,Mnu,b1,b2,bs = thy_args
->>>>>>> 42ed741 (mask making scripts)
    if k is None: k = ks
    cosmo = getCosmo(thy_args)
    h     = cosmo.h()
@@ -149,27 +99,13 @@ def pggVelocileptors(thy_args,z,k=None):
    plin  = np.array([cosmo.pk_cb_lin(kk*h,z)*h**3 for kk in klin])
    cleft = CLEFT(klin,plin,cutoff=5.)
    cleft.make_ptable(kmin=min(k),kmax=max(k),nk=len(k))
-   kout,za    = cleft.pktable[:,0],cleft.pktable[:,13]   
-<<<<<<< HEAD
-   res        = np.zeros((len(kout),17))
-   res[:,:13] = cleft.pktable[:,:13]
-   res[:,13]  = np.ones_like(kout)
-   res[:,14]  = -0.5*kout**2*za
-   return res
-
-   
-=======
-   #res        = np.zeros((len(kout),17))
-   #res[:,:13] = cleft.pktable[:,:13]
-   #res[:,13]  = np.ones_like(kout)
-   #res[:,14]  = -0.5*kout**2*za
+   kout,za  = cleft.pktable[:,0],cleft.pktable[:,13]   
    res      = np.zeros((len(kout),3))
    res[:,0] = kout
    res[:,1] = cleft.combine_bias_terms_pk(b1, b2, bs, 0., 0., 0.)[1]
    res[:,2] = -0.5*kout**2*za
    return res
 
->>>>>>> 42ed741 (mask making scripts)
 def pgmVelocileptors(thy_args,z,k=None):
    """
    Returns a Pgm table with shape (Nk,7).
@@ -191,10 +127,7 @@ def pgmVelocileptors(thy_args,z,k=None):
       wavevectors [h/Mpc] on which to evaluate
       the power spectrum table
    """
-<<<<<<< HEAD
-=======
    omb,omc,ns,ln10As,H0,Mnu,b1,b2,bs = thy_args
->>>>>>> 42ed741 (mask making scripts)
    if k is None: k = ks
    cosmo = getCosmo(thy_args)
    h     = cosmo.h()
@@ -205,27 +138,6 @@ def pgmVelocileptors(thy_args,z,k=None):
    cleft = CLEFT(klin,plin,cutoff=5.)
    cleft.make_ptable(kmin=min(k),kmax=max(k),nk=len(k))
    kout,za  = cleft.pktable[:,0],cleft.pktable[:,13]
-<<<<<<< HEAD
-   res      = np.zeros((len(kout),7))
-   res[:,0] = kout                     # k
-   res[:,1] = cleft.pktable[:,1 ]      # 1
-   res[:,2] = cleft.pktable[:,2 ]/2.   # b1
-   res[:,3] = cleft.pktable[:,4 ]/2.   # b2
-   res[:,4] = cleft.pktable[:,7 ]/2.   # bs
-   res[:,5] = cleft.pktable[:,11]/2.   # b3
-   res[:,6] = -0.5*kout**2*za          # alphaX
-   return res
-   
-   
-=======
-   #res      = np.zeros((len(kout),7))
-   #res[:,0] = kout                     # k
-   #res[:,1] = cleft.pktable[:,1 ]      # 1
-   #res[:,2] = cleft.pktable[:,2 ]/2.   # b1
-   #res[:,3] = cleft.pktable[:,4 ]/2.   # b2
-   #res[:,4] = cleft.pktable[:,7 ]/2.   # bs
-   #res[:,5] = cleft.pktable[:,11]/2.   # b3
-   #res[:,6] = -0.5*kout**2*za          # alphaX
    res      = np.zeros((len(kout),3))
    res[:,0] = kout
    res[:,1] = cleft.combine_bias_terms_pk_crossmatter(b1, b2, bs, 0., 0.)[1]
@@ -307,102 +219,3 @@ def pggHEFT(thy_args,z):
    res[:,1]  = np.dot(T[:,1:],bterms_gg) # bias-contribution
    res[:,2]  = -0.5 * T[:,0]**2 * T[:,2] # counterterm
    return res  
-
-
-# some old code that may eventually be trashed? 
-'''
->>>>>>> 42ed741 (mask making scripts)
-import jax.numpy as jnp
-# OBVIOUSLY NEED TO REMOVE HARD-CODED PATH
-# IN FAVOR OF PIP INSTALLATION. HOWEVER, 
-# THE CURRENT GITHUB DOESN'T HAVE NN WEIGHTS
-# AND ALSO IS NOT JAX-COMPATIBLE, WHICH IS WHY I'm 
-# LOADING MY OWN REPO (GOT WEIGHTS FROM JOE, AND 
-# MODIFIED heft_emu -> heft_emu_jax)
-import sys
-sys.path.append('/home/noah/Berkeley/aemulus_heft') # running on my laptop since perlmutter is a pos
-from aemulus_heft.heft_emu_jax import NNHEFTEmulator
-
-nnemu = NNHEFTEmulator()
-
-def pmmHEFT(thy_args,z):
-   """
-   Assumes thy_args[:5] = [omb,omc,ns,As,H0] and z = ndarray
-   
-   Returns res = (Nk,1+Nz) table where the first column is k and the
-   remaining Nz columns are the matter power spectrum evaluated
-   at each z.
-   """
-   omb,omc,ns,As,H0 = thy_args[:5]
-   cosmo = jnp.zeros((len(z),8))
-   cosmo = cosmo.at[:,-1].set(z)
-   cosmo = cosmo.at[:,:-1].set([omb, omc, -1., ns, As, H0, 0.06])
-   k_nn, spec_heft_nn = nnemu.predict(cosmo)
-   res = jnp.zeros((len(k_nn),len(z)+1))
-   res = res.at[:,0].set(k_nn)
-   res = res.at[:,1:].set(jnp.swapaxes(spec_heft_nn[:,0,:],0,1))
-   return res
-
-def ptableHEFT(thy_args,z):
-   """
-   Assumes thy_args[:5] = [omb,omc,ns,As,H0] and z = float
-   
-   Returns monomial table = (Nk,1+Nmono) ndarray. The first column is k, 
-   while the order of the 15 monomials is:
-   
-   1-1, 1-cb, cb-cb, delta-1, delta-cb, delta-delta, delta2-1, delta2-cb, 
-   delta2-delta, delta2-delta2, s2-1, s2-cb, s2-delta, s2-delta2, s2-s2.
-   """
-   omb,omc,ns,As,H0 = thy_args[:5]
-   cosmo = jnp.atleast_2d([omb, omc, -1., ns, As, H0, 0.06, z])
-   k_nn, spec_heft_nn = nnemu.predict(cosmo)
-   Nmono = spec_heft_nn.shape[1]
-   res = jnp.zeros((len(k_nn),Nmono+1))
-   res = res.at[:,0].set(k_nn)
-   res = res.at[:,1:].set(jnp.swapaxes(spec_heft_nn[0,:,:],0,1))
-   #for i in range(Nmono): res = res.at[:,i+1].set(spec_heft_nn[0,i,:])
-   return res
-   
-def pgmHEFT(thy_args,z):
-   """
-   Assumes thy_args = [omb,omc,ns,As,H0,b1,b2,bs] and z = float
-   
-   Returns res = (Nk,3) ndarray, where the first column is k, the second column
-   is the "bias contribution" (i.e. terms that cannot be analytically 
-   marginalized over), while the third column is k^2 P_{cb, 1}
-   
-   The full prediction is res[:,1] + alpha_x * res[:,2]
-   """
-   omb,omc,ns,As,H0,b1,b2,bs = thy_args
-   bterms_gm = jnp.array([0, 1, 0, b1, 0, 0, 0.5*b2, 0, 0, 0, bs, 0, 0, 0, 0])
-   T   = ptableHEFT(thy_args[:5],z)
-   res = jnp.zeros((T.shape[0],3))
-   res = res.at[:,0].set(T[:,0])
-   res = res.at[:,1].set( jnp.dot(T[:,1:],bterms_gm) ) # bias-contribution
-   res = res.at[:,2].set(-0.5 * T[:,0]**2 * T[:,1])    # counterterm
-   return res
-   
-def pggHEFT(thy_args,z):
-   """
-   Assumes thy_args = [omb,omc,ns,As,H0,b1,b2,bs] and z = float
-   
-   Returns res = (Nk,4) ndarray, where the first column is k, the second column
-   is the "bias contribution" (i.e. terms that cannot be analytically 
-   marginalized over), the third column is k^2 P_{cb, cb}, while the 
-   fourth column is the shot noise contribution (ones)
-   
-   The full prediction is res[:,1] + alpha_0 * res[:,2] + SN * res[:,3]
-   """
-   omb,omc,ns,As,H0,b1,b2,bs = thy_args
-   bterms_gg = jnp.array([0, 0, 1, 0, 2*b1, b1**2, 0, b2, b2*b1, 0.25*b2**2, 0, 2*bs, 2*bs*b1, bs*b2, bs**2])
-   T   = ptableHEFT(thy_args[:5],z)
-   res = jnp.ones((T.shape[0],4))
-   res = res.at[:,0].set(T[:,0])
-   res = res.at[:,1].set( jnp.dot(T[:,1:],bterms_gg) ) # bias-contribution
-   res = res.at[:,2].set(-0.5 * T[:,0]**2 * T[:,2])    # counterterm
-<<<<<<< HEAD
-   return res
-=======
-   return res
-'''
->>>>>>> 42ed741 (mask making scripts)
