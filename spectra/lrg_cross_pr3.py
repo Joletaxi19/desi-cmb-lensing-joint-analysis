833b4c2548bc41c42467a6f3234650a9ded3cdd1
Jonathan Hawla <jonathan.hawla@icloud.com>
2025-04-23 09:15:30 -0700

    fix: correction des chemins d'accès pour les fichiers de cartes et de masques

--- spectra/lrg_cross_pr3.py
+++ spectra/lrg_cross_pr3.py
@@ -5,8 +5,8 @@ from globe      import LEDGES,NSIDE
 from do_mc_corr import apply_mc_corr
 
 # load maps, masks and CMB lensing noise curves
-lrg_maps  = [hp.read_map(f'../maps/lrg_s0{isamp}_del.hpx2048.fits') for isamp in range(1,5)]
-lrg_masks = [hp.read_map(f'../maps/masks/lrg_s0{isamp}_msk.hpx2048.fits') for isamp in range(1,5)]
+lrg_maps  = [hp.read_map(f'../maps/lrg_s0{isamp}_del.hpx2048.public.fits.gz') for isamp in range(1,5)]
+lrg_masks = [hp.read_map(f'../maps/masks/lrg_s0{isamp}_msk.hpx2048.public.fits.gz') for isamp in range(1,5)]
 pr3_map   = [hp.read_map(f'../maps/PR3_lens_kap_filt.hpx2048.fits')]
 pr3_mask  = [hp.read_map(f'../maps/masks/PR3_lens_mask.fits')]
 pr3_nkk   = np.loadtxt(f'../data/PR3_lens_nlkk_filt.txt')
@@ -29,5 +29,5 @@ pairs = [[0,1],[0,2],[0,3],[0,4],[1,1],[2,2],[3,3],[4,4]]
 full_master(LEDGES,maps,msks,names,fnout,cij=cij,do_cov=True,pairs=pairs)
 
 # apply MC correction
-bdir = '../mc_correction/sims/'
-apply_mc_corr(fnout,fnout,kapNames[0],galNames,[bdir+'lrg-full-z1_PR3-baseline']*4)
+# bdir = '../mc_correction/sims/'
+# apply_mc_corr(fnout,fnout,kapNames[0],galNames,[bdir+'lrg-full-z1_PR3-baseline']*4)
